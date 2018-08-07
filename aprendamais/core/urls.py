088c6f257a7dc85d99b59be2f4d85b40c98b38f9
Joberth Rogers <joberth.rogers18@gmail.com>
2018-08-07 19:37:24 -0300

    Define the route to the main page
    Stylisheet the main page of site

--- aprendamais/core/urls.py
+++ aprendamais/core/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.HomeTemplateView.as_view(), name='home'),
 ]
--- aprendamais/core/views.py
+++ aprendamais/core/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Hello World')
+class HomeTemplateView(TemplateView):
+    template_name='home.html'
 
