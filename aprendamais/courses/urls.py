381e3e5e88d447a8bb2138d0f7f3ff2c97f102dc
Joberth Rogers <joberth.rogers18@gmail.com>
2018-08-12 23:42:06 -0300

    Adding form to create course

--- aprendamais/courses/urls.py
+++ aprendamais/courses/urls.py
@@ -5,5 +5,6 @@ app_name = 'courses'
 
 urlpatterns = [
     path('', views.CourseListView.as_view(), name='index'),
+    path('novo/', views.CourseCreateView.as_view(), name='create_course'),
     #path('contato/', views.ContactTemplateView.as_view(), name='contact')
 ]
--- aprendamais/courses/views.py
+++ aprendamais/courses/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import CreateView
 from .models import Course
+from django.urls import reverse_lazy
 
 class CourseListView(ListView):
     model = Course
     template_name = 'courses/index.html'
 
+class CourseCreateView(CreateView):
+    model = Course
+    template_name = 'courses/create_course.html'
+    fields = [
+        'name',
+        'description',
+        'image',
+    ]
+
+    success_url = reverse_lazy(
+        viewname = 'courses:index',
+    )
